fa707eacb4b1395034ad625bb4043c763d4f86bc
angre-kim <angre@daum.net>
2020-02-14 13:33:53 +0900

    최종

---
+++ lib/services/weather.dart
@@ -0,0 +1,65 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
+const apiKey = '83b6363fa422a666a28b75dea9ebc7a6';
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
+class WeatherModel {
+
+  Future<dynamic> getCityWeather(String cityName) async{
+    //입력한 도시명으로 API 가져오기
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    try {
+      await location.getCurrentLocation().timeout(Duration(seconds: 5));
+
+      NetworkHelper networkHelper = NetworkHelper(
+          '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+      var weatherData = await networkHelper.getData();
+      return weatherData;
+    } catch (e) {
+      var weatherData = null;
+      return weatherData;
+    }
+  }
+
+  String getWeatherIcon(int condition) {
+    if (condition < 300) {
+      return '🌩';
+    } else if (condition < 400) {
+      return '🌧';
+    } else if (condition < 600) {
+      return '☔️';
+    } else if (condition < 700) {
+      return '☃️';
+    } else if (condition < 800) {
+      return '🌫';
+    } else if (condition == 800) {
+      return '☀️';
+    } else if (condition <= 804) {
+      return '☁️';
+    } else {
+      return '🤷‍';
+    }
+  }
+
+  String getMessage(int temp) {
+    if (temp > 25) {
+      return 'It\'s 🍦 time';
+    } else if (temp > 20) {
+      return 'Time for shorts and 👕';
+    } else if (temp < 10) {
+      return 'You\'ll need 🧣 and 🧤';
+    } else {
+      return 'Bring a 🧥 just in case';
+    }
+  }
+}
